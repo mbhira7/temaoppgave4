67700ee07ba22e2a95a332f1c6456f5e78d66a7e
mbhira7 <54266598+mbhira7@users.noreply.github.com>
2019-12-04 22:06:21 +0100

    Updates

---
+++ precache-manifest.ea3e2d595efd48a1ff56a27ad05c888a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259d7a3746488f5ff3a9cdfc83dc2cdf",
+    "url": "/temaoppgave4/index.html"
+  },
+  {
+    "revision": "65796cdb993a2601650a",
+    "url": "/temaoppgave4/static/css/main.d6d54f35.chunk.css"
+  },
+  {
+    "revision": "82d3a04f3044a25b6d0d",
+    "url": "/temaoppgave4/static/js/2.f4c14980.chunk.js"
+  },
+  {
+    "revision": "65796cdb993a2601650a",
+    "url": "/temaoppgave4/static/js/main.11086987.chunk.js"
+  },
+  {
+    "revision": "5d29fe4a45d53360491a",
+    "url": "/temaoppgave4/static/js/runtime-main.9e178eb1.js"
+  },
+  {
+    "revision": "2274ce99ef721c610b7f2b9ba3551205",
+    "url": "/temaoppgave4/static/media/logo.2274ce99.png"
+  }
+]);
